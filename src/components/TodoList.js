1aeb63d07341f009565083e6f0c3b46fded9e6e4
hadi <hadi_960@hotmail.com>
2021-09-30 23:17:02 -0500

    passed prop to its parents and set the state

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -2,11 +2,13 @@ import React from "react";
 import Todo from './Todo'
 
 const TodoList = ({todos}) => {
-  console.log(todos)
+  // console.log(todos)
     return(
         <div className="todo-container">
       <ul className="todo-list">
-      
+      {todos.map(todo => (
+        <Todo text={todo.text} />
+      ))}
       </ul>
     </div>
     )
